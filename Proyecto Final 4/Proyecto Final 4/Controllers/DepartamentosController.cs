c13308397b48de48f7272214911692aae3700455
Erickmanuelh <manuel.hasbun@hotmail.com>
2019-04-05 21:21:10 -0400

    Nomina

---
+++ Proyecto Final 4/Proyecto Final 4/Controllers/DepartamentosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Final_4.Models;
+
+namespace Proyecto_Final_4.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        // GET: Departamentos
+        public ActionResult Index()
+        {
+            return View(db.DepartamentosSet.ToList());
+        }
+
+        // GET: Departamentos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamentos departamentos = db.DepartamentosSet.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CodigoD,NombreD")] Departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DepartamentosSet.Add(departamentos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamentos departamentos = db.DepartamentosSet.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: Departamentos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CodigoD,NombreD")] Departamentos departamentos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamentos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamentos);
+        }
+
+        // GET: Departamentos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Departamentos departamentos = db.DepartamentosSet.Find(id);
+            if (departamentos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamentos);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Departamentos departamentos = db.DepartamentosSet.Find(id);
+            db.DepartamentosSet.Remove(departamentos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Proyecto Final 4/Proyecto Final 4/Controllers/NominasController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_Final_4.Models;
+
+namespace Proyecto_Final_4.Controllers
+{
+    public class NominasController : Controller
+    {
+        private Model1Container db = new Model1Container();
+
+        public ActionResult Totalizar()
+        {
+            ViewBag.TotalSalario = db.EmpleadosSet.Sum(a => a.Salario);
+            ViewBag.TotalEmpleados = db.EmpleadosSet.Count();
+
+         //   var query = (from a in db.EmpleadosSet
+         //                select a);
+
+            db.SaveChanges();
+            return View();
+        }
+
+
+        // GET: Nominas
+        public ActionResult Index()
+        {
+            return View(db.NominasSet.ToList());
+        }
+
+        // GET: Nominas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.NominasSet.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // GET: Nominas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Nominas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Anio,Mes,Monto")] Nominas nominas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.NominasSet.Add(nominas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nominas);
+        }
+
+        // GET: Nominas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.NominasSet.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // POST: Nominas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Anio,Mes,Monto")] Nominas nominas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nominas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nominas);
+        }
+
+        // GET: Nominas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.NominasSet.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // POST: Nominas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nominas nominas = db.NominasSet.Find(id);
+            db.NominasSet.Remove(nominas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
